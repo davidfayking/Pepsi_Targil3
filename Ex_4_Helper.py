a5df994bfae99d3ee94887885bc83e24d2205387
David Fay <david0524261414@gmail.com>
2024-05-31 15:29:08 +0300

    Initial commit

---
+++ Ex_4_Helper.py
@@ -0,0 +1,155 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+g = 9.8
+theta_israel = (np.pi / 6)
+omega_tot = 2 * np.pi / (24 * 3600)  # omega of earth
+omega_x = 0
+omega_y = omega_tot * np.cos(theta_israel)
+omega_z = omega_tot * np.sin(theta_israel)
+alpha = 0.05
+R_base = 200
+
+
+def acceleration(v_x, v_y, v_z, m, A):
+    a_x = - (alpha * A * np.sqrt(v_x ** 2 + v_y ** 2 + v_z ** 2) * v_x / m + 2 * (omega_y * v_z - v_y * omega_z))
+    a_y = - (alpha * A * np.sqrt(v_x ** 2 + v_y ** 2 + v_z ** 2) * v_y / m + 2 * (v_x * omega_z - omega_x * v_z))
+    a_z = - (g + (alpha * A * np.sqrt(v_x ** 2 + v_y ** 2 + v_z ** 2) * v_z / m) + 2 * (v_y * omega_x - omega_y * v_x))
+    return a_x, a_y, a_z
+
+
+def runge_kutta_rocket(r_0=(0, 0, 0), dt=0.01, N=10000, m=500,v_0=(500 * np.cos(np.radians(20)) * np.sin(np.radians(-90)),
+                            500 * np.cos(np.radians(-90)) * np.cos(np.radians(20)),
+                            500 * np.sin(np.radians(20))), A=1, sensitivity=0.01):
+    t = np.linspace(start=0, num=N, stop=0 + (N - 1) * dt)
+    x = np.ndarray(N)
+    y = np.ndarray(N)
+    z = np.ndarray(N)
+    vx = np.ndarray(N)
+    vy = np.ndarray(N)
+    vz = np.ndarray(N)
+
+    x[0], y[0], z[0] = r_0
+    vx[0], vy[0], vz[0] = v_0
+
+    for i in range(N - 1):
+        x[i + 1], y[i + 1], z[i + 1], vx[i + 1], vy[i + 1], vz[i + 1] = runga_step(x[i], y[i], z[i], vx[i], vy[i],
+                                                                                   vz[i], m, A, dt)
+
+        if (z[i + 1] < 0):
+            break
+
+    i_ground = np.argmax(z < 0.)
+
+    t[i_ground], x[i_ground], y[i_ground], z[i_ground] = final_step_to_ground_no_randomness(
+        (x[i_ground - 1], y[i_ground - 1], z[i_ground - 1]), (x[i_ground - 2], y[i_ground - 2], z[i_ground - 2]),
+        (vx[i_ground - 1], vy[i_ground - 1], vz[i_ground - 1]), sensitivity, m, A, t[i_ground - 1], t[i_ground - 2])
+    return t[:i_ground + 1], (x[:i_ground + 1], y[:i_ground + 1], z[:i_ground + 1]), i_ground
+
+
+def runga_step(x, y, z, v_x, v_y, v_z, m, A, dt):
+    k1_x, k1_y, k1_z = v_x, v_y, v_z
+    k1_vx, k1_vy, k1_vz = acceleration(v_x, v_y, v_z, m, A)
+
+    k2_x, k2_y, k2_z = (v_x + (dt / 2) * k1_vx), (v_y + (dt / 2) * k1_vy), (v_z + (dt / 2) * k1_vz)
+    k2_vx, k2_vy, k2_vz = acceleration((v_x + (dt / 2) * k1_vx), (v_y + (dt / 2) * k1_vy),
+                                       (v_z + (dt / 2) * k1_vz), m, A)
+
+    k3_x, k3_y, k3_z = (v_x + (dt / 2) * k2_vx), (v_y + (dt / 2) * k2_vy), (v_z + (dt / 2) * k2_vz)
+    k3_vx, k3_vy, k3_vz = acceleration((v_x + (dt / 2) * k2_vx), (v_y + (dt / 2) * k2_vy),
+                                       (v_z + (dt / 2) * k2_vz), m, A)
+
+    k4_x, k4_y, k4_z = (v_x + dt * k3_vx), (v_y + dt * k3_vy), (v_z + dt * k3_vz)
+    k4_vx, k4_vy, k4_vz = acceleration((v_x + dt * k3_vx), (v_y + dt * k3_vy), (v_z + dt * k3_vz), m, A)
+
+    # recurssion step
+    x_return, y_return, z_return = (x + 1 / 6 * (k1_x + 2 * k2_x + 2 * k3_x + k4_x) * dt), (
+            y + 1 / 6 * (k1_y + 2 * k2_y + 2 * k3_y + k4_y) * dt), (
+                                           z + 1 / 6 * (k1_z + 2 * k2_z + 2 * k3_z + k4_z) * dt)
+
+    vx_return, vy_return, vz_return = (v_x + 1 / 6 * (k1_vx + 2 * k2_vx + 2 * k3_vx + k4_vx) * dt), (
+            v_y + 1 / 6 * (k1_vy + 2 * k2_vy + 2 * k3_vy + k4_vy) * dt), (
+                                              v_z + 1 / 6 * (k1_vz + 2 * k2_vz + 2 * k3_vz + k4_vz) * dt)
+
+    return x_return, y_return, z_return, vx_return, vy_return, vz_return
+
+
+def final_step_to_ground_no_randomness(r_curr, r_prev, v_curr, sensitivity, m, A, t_curr, t_prev):
+    r = np.array(r_curr)
+    r_old = np.array(r_prev)
+    vx, vy, vz = v_curr
+    t = t_curr
+    t_old = t_prev
+    while abs(r[2]) > sensitivity:
+        dt = - r[2] * (t - t_old) / (r[2] - r_old[2])
+        t_old = t
+        t += dt
+        r_old = np.array([r[0], r[1], r[2]])
+        r[0], r[1], r[2], vx, vy, vz = runga_step(r[0], r[1], r[2], vx, vy, vz, m, A, dt)
+    return t, r[0], r[1], r[2]
+
+
+def acceleration_with_random_and_with_correction(v_x, v_y, v_z, m, A, dt, correction, beta=1000):
+    theta, phi = get_angles_from_v((v_x, v_y, v_z))
+    a_correction_z = (correction[1] * np.cos(theta)) / m
+    a_correction_x = (- correction[1] * np.sin(theta) * np.sin(phi) + correction[0] * np.cos(phi)) / m
+    a_correction_y = (- correction[1] * np.sin(theta) * np.cos(phi) - correction[0] * np.sin(phi)) / m
+    fr_x, fr_y, fr_z = random_vector()
+    inverse_dt = 1 / np.sqrt(dt / 4)
+    fr_x, fr_y, fr_z = fr_x * beta * A * inverse_dt / m, fr_y * beta * A * inverse_dt / m, fr_z * beta * A * inverse_dt / m
+    a_x = fr_x + a_correction_x - (
+            alpha * A * np.sqrt(v_x ** 2 + v_y ** 2 + v_z ** 2) * v_x / m + 2 * (omega_y * v_z - v_y * omega_z))
+    a_y = fr_y + a_correction_y - (
+            alpha * A * np.sqrt(v_x ** 2 + v_y ** 2 + v_z ** 2) * v_y / m + 2 * (v_x * omega_z - omega_x * v_z))
+    a_z = fr_z + a_correction_z - (g + (alpha * A * np.sqrt(v_x ** 2 + v_y ** 2 + v_z ** 2) * v_z / m) + 2 * (
+            v_y * omega_x - omega_y * v_x))
+    return a_x, a_y, a_z
+
+
+def random_vector():
+    return (np.random.normal(), np.random.normal(), np.random.normal())
+
+
+def runga_step_with_correction(x, y, z, v_x, v_y, v_z, m, A, dt, correction, beta):
+    k1_x, k1_y, k1_z = v_x, v_y, v_z
+    k1_vx, k1_vy, k1_vz = acceleration_with_random_and_with_correction(v_x, v_y, v_z, m, A, dt, correction, beta=beta)
+
+    k2_x, k2_y, k2_z = (v_x + (dt / 2) * k1_vx), (v_y + (dt / 2) * k1_vy), (v_z + (dt / 2) * k1_vz)
+    k2_vx, k2_vy, k2_vz = acceleration_with_random_and_with_correction((v_x + (dt / 2) * k1_vx),
+                                                                       (v_y + (dt / 2) * k1_vy),
+                                                                       (v_z + (dt / 2) * k1_vz), m, A, dt, correction,
+                                                                       beta=beta)
+
+    k3_x, k3_y, k3_z = (v_x + (dt / 2) * k2_vx), (v_y + (dt / 2) * k2_vy), (v_z + (dt / 2) * k2_vz)
+    k3_vx, k3_vy, k3_vz = acceleration_with_random_and_with_correction((v_x + (dt / 2) * k2_vx),
+                                                                       (v_y + (dt / 2) * k2_vy),
+                                                                       (v_z + (dt / 2) * k2_vz), m, A, dt, correction,
+                                                                       beta=beta)
+
+    k4_x, k4_y, k4_z = (v_x + dt * k3_vx), (v_y + dt * k3_vy), (v_z + dt * k3_vz)
+    k4_vx, k4_vy, k4_vz = acceleration_with_random_and_with_correction((v_x + dt * k3_vx), (v_y + dt * k3_vy),
+                                                                       (v_z + dt * k3_vz), m, A, dt, correction,
+                                                                       beta=beta)
+
+    # recurssion step
+    x_return, y_return, z_return = (x + 1 / 6 * (k1_x + 2 * k2_x + 2 * k3_x + k4_x) * dt), (
+            y + 1 / 6 * (k1_y + 2 * k2_y + 2 * k3_y + k4_y) * dt), (
+                                           z + 1 / 6 * (k1_z + 2 * k2_z + 2 * k3_z + k4_z) * dt)
+
+    vx_return, vy_return, vz_return = (v_x + 1 / 6 * (k1_vx + 2 * k2_vx + 2 * k3_vx + k4_vx) * dt), (
+            v_y + 1 / 6 * (k1_vy + 2 * k2_vy + 2 * k3_vy + k4_vy) * dt), (
+                                              v_z + 1 / 6 * (k1_vz + 2 * k2_vz + 2 * k3_vz + k4_vz) * dt)
+
+    return x_return, y_return, z_return, vx_return, vy_return, vz_return
+
+
+def get_angles_from_v(v):
+    """
+    :param v:
+    :return: returns the theta and phi elements of the velocity
+    """
+    theta = np.arcsin(v[2] / np.sqrt(v[0] ** 2 + v[1] ** 2 + v[2] ** 2))
+    phi = np.arctan2(v[0], v[1])
+    return theta, phi
---
+++ Ex_4_Template.py
@@ -0,0 +1,161 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import Ex_4_Helper as helper
+
+g = 9.8
+theta_israel = (np.pi / 6)
+omega_tot = 2 * np.pi / (24 * 3600)  # omega of earth
+omega_x = 0
+omega_y = omega_tot * np.cos(theta_israel)
+omega_z = omega_tot * np.sin(theta_israel)
+alpha = 0.05
+R_base = 200
+
+
+def get_landing_spot(r, v, dt=0.1, N=10000, m=500, A=1, sensitivity=0.01):
+    t, r_land, i = helper.runge_kutta_rocket(r_0=r, dt=dt, N=N, m=m, v_0=v, A=A, sensitivity=sensitivity)
+    return r_land[0], r_land[1]
+
+
+def get_angles_from_v(v):
+    """
+    :param v:
+    :return: returns the theta and phi elements of the velocity
+    """
+    theta = np.arcsin(v[2] / np.sqrt(v[0] ** 2 + v[1] ** 2) + v[2] ** 2)
+    phi = np.arctan2(v[0], v[1])
+    return theta, phi
+
+
+def runge_kutta_rocket_with_random_and_correction(r_0=(0, 0, 0), v_0=500, dt=0.1, N=10000, m=500, theta=np.radians(20),
+                                                  phi=np.radians(-90), A=1, sensitivity=0.01, beta=1000):
+    """
+    :param r_0: starting location of the rocket
+    :param v_0: starting velocity of the rocket
+    :param dt: time interval
+    :param N: maximum number of iterations
+    :param m: the rocket's mass
+    :param theta: angle of launch in respective to the ground
+    :param phi: angle of launch in respective to the north
+    :param A: the rocket's cross-sectional Area
+    :param sensitivity: a parameter of the simulation - leave it as it is.
+    :param beta: the coefficient of the random force the rocket gets.
+    :return: t - an array of the time,
+             r - a 3 tuple of three numpy arrays containing the coordinates of the rocket at each time,
+             i - the last index in the arrays - (len(array) - 1)
+
+    """
+    t = np.linspace(start=0, num=N, stop=0 + (N - 1) * dt)
+    x = np.ndarray(N)
+    y = np.ndarray(N)
+    z = np.ndarray(N)
+    vx = np.ndarray(N)
+    vy = np.ndarray(N)
+    vz = np.ndarray(N)
+
+
+    x[0], y[0], z[0] = r_0
+    vx[0], vy[0], vz[0] = v_0 * np.sin(phi) * np.cos(theta), v_0 * np.cos(phi) * np.cos(theta), v_0 * np.sin(theta)
+
+    x_tearget, y_target = get_landing_spot((x[0], y[0], z[0]), (vx[0], vy[0], vz[0]), dt=dt, N=N, m=m, A=A,
+                                           sensitivity=sensitivity)
+    correction = (0, 0)
+    theoretical_t, theoretical_r, _ = helper.runge_kutta_rocket(dt=delta_t, N=n)
+    for i in range(N - 1):
+        x[i + 1], y[i + 1], z[i + 1], vx[i + 1], vy[i + 1], vz[i + 1] = helper.runga_step_with_correction(x[i], y[i],
+                                                                                                          z[i],
+                                                                                                          vx[i], vy[i],
+                                                                                                          vz[i], m, A,
+                                                                                                          dt,
+                                                                                                          correction=correction,
+                                                                                                          beta=beta)
+
+
+        # you may create whatever variables you want in runge_kutta_rocket_with_random_and_correction
+        # and have calculate_correction function get whatever parameters you want.
+
+        current_position = (x[i], y[i ], z[i])
+        current_velocity = (vx[i ], vy[i ], vz[i ])
+        correction = calculate_correction(current_position,current_velocity,theoretical_r,dt)
+        if (z[i + 1] < 0):
+            break
+
+    i_ground = np.argmax(z < 0.)
+
+    t[i_ground], x[i_ground], y[i_ground], z[i_ground] = helper.final_step_to_ground_no_randomness(
+        (x[i_ground - 1], y[i_ground - 1], z[i_ground - 1]), (x[i_ground - 2], y[i_ground - 2], z[i_ground - 2]),
+        (vx[i_ground - 1], vy[i_ground - 1], vz[i_ground - 1]), sensitivity, m, A, t[i_ground - 1], t[i_ground - 2])
+    return t[:i_ground + 1], (x[:i_ground + 1], y[:i_ground + 1], z[:i_ground + 1]), i_ground
+
+
+# create this function so that the location of impact is within 20 meters of the target
+Kp = 0.8
+# Target position
+target_x = -8394.459213691083
+target_y = 3.436456836839949e-06
+target_radius = 10
+def calculate_correction(current_pos, current_velocity,theoretical_pos, dt =0.1, target_position=(target_x, target_y)):
+    # return (0, 0)
+    """
+       Calculate the correction forces based on the current position and velocity.
+
+       :param current_position: Current position of the missile
+       :param current_velocity: Current velocity of the missile
+       :param dt: Time step of the simulation
+       :return: Tuple of (horizontal force, vertical force)
+       """
+    # Calculate errors in x and y directions
+    # Calculate the landing spot based on current position and velocity
+    x, y, z = current_pos
+    vx, vy, vz = current_velocity
+    theoretical_positions = np.array(theoretical_pos).T
+    mask = theoretical_positions[:, 0] <= x
+    if not np.any(mask):
+        return (0, 0)  # If no such point exists, no correction is applied
+    theoretical_positions = theoretical_positions[mask]
+    # distances = np.linalg.norm(theoretical_positions - np.array([x, y, z]), axis=1)
+    distances = np.linalg.norm(theoretical_positions[:, [0, 2]] - np.array([x, z]), axis=1)
+    nearest_index = np.argmin(distances)
+    x_theoretical, y_theoretical, z_theoretical = theoretical_positions[nearest_index]
+    error_x = x_theoretical - x
+    error_y = y_theoretical - y
+    error_z = z_theoretical - z
+    # Proportional control
+    correction_x = Kp * error_x
+    correction_y = Kp * error_y
+    correction_z = Kp * error_z
+
+    theta, phi = helper.get_angles_from_v(current_velocity / np.sqrt(vx**2 + vy**2 + vz**2))
+
+    # Decompose correction forces into engine forces
+    # f_0 = correction_x * (1/np.cos(phi)) + correction_z * np.tan(theta)*np.tan(phi)
+    # f_1=correction_z/np.cos(theta)
+    f_0 = correction_x / np.cos(phi)
+    f_1 =0
+    # Decompose correction forces into x, y, z components
+    f_x = -f_1 * np.sin(theta) * np.sin(phi) + f_0 * np.cos(phi)
+    f_y = -f_1 * np.sin(theta) * np.cos(phi) - f_0 * np.sin(phi)
+    f_z = f_1 * np.cos(theta)
+
+    # Return the correction tuple (f_0, f_1)
+    max_force = 1000000
+    f_0 = np.clip(f_0,-max_force,max_force)
+    f_1 = np.clip(f_1,-max_force,max_force)
+    return (f_0, f_1)
+
+if __name__ == '__main__':
+    n = 10000
+    delta_t = 0.5
+    t, r, i = helper.runge_kutta_rocket(dt=delta_t, N=n)
+    plt.plot(r[0], r[2])
+    print(r[0][-1], r[2][-1])
+    # plt.show()
+
+    n = 10000
+    delta_t = 0.5
+    t, r, i = runge_kutta_rocket_with_random_and_correction(dt=delta_t, N=n)
+    plt.plot(r[0], r[2])
+    print(r[0][-1], r[2][-1])
+    plt.show()
